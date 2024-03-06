c9527d6c986342baa7ae7ec5053f5e328aba9fb4
Amitai <amitai333@gmail.com>
2024-03-06 10:42:17 -0500

    Remove gzip

--- deploy.js
+++ deploy.js
@@ -40,7 +40,6 @@ let deploy = function (params) {
                         --cwd ./ \
                         ${distIdArg} \
                         --etag \
-                        --gzip xml,html,htm,js,css,ttf,otf,svg,txt \
                         ${cacheFlag} \
                         ${invalidationArg} \
                         ${deleteRemovedArg} \
